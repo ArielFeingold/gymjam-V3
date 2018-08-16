5e6c5ce62a178961b328fea5d8da0a4bf4ca95ea
ArielFeingold <feingold.ariel@gmail.com>
2018-08-16 06:40:42 -0400

    established api proxy changed password_confirm to password_confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2018_08_16_100453) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "username"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
